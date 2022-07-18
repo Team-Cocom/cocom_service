0ee7c49985ffb83ce462d248d207674997993f21
yuno <chldh1533@gamil.com>
2022-07-18 16:49:59 +0900

    초기상태

---
+++ src/main/java/com/greenart/cocom_service/CocomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.greenart.cocom_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocomServiceApplication.class, args);
+	}
+
+}
